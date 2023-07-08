8631450b55fabf6f2eebc50c878529a3923ff337
SammaraIngrid <sammaracavalcante2@gmail.com>
2023-07-08 17:47:44 -0300

    add validates em users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   belongs_to :family
+  validates :name, presence: true
   validates :birthday, presence: true
+  validates :surname, presence: true
 
   def age
     Date.current.year - self.birthday.year
